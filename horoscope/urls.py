eeec81e3d62fe5ec557c7a6bcb5da1e56fe3f646
Landysh <zenladysh@gmail.com>
2022-03-28 23:28:53 +0300

    add info about zodiac by date

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.index),
     path('type/', views.get_types_zodiac, name='horoscope_type'),
     path('type/<str:type_zodiacs>', views.get_zodiacs_by_type),
+    path('<int:month>/<int:day>/', views.get_info_about_sign_zodiac_by_date),
     path('<int:sign_zodiac>/', views.get_info_about_sign_zodiac_by_number),
     path('<str:sign_zodiac>/', views.get_info_about_sign_zodiac, name='horoscope_name'),
-    path('<int:date_month>/<int:date_day>/', views.get_info_about_sign_zodiac_by_date),
 ]
--- horoscope/views.py
+++ horoscope/views.py
@@ -1,20 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from collections import namedtuple
+from datetime import datetime as dt
 
+Zodiac = namedtuple("Zodiac", "description start_date end_date")
 zodiac_dict = {
-    "aries": "Овен - первый знак зодиака, планета Марс (с 21 марта по 20 апреля)",
-    "taurus": "Телец - второй знак зодиака, планета Венера (с 21 апреля по 21 мая)",
-    "gemini": "Близнецы - третий знак зодиака, планета Меркурий (с 22 мая по 21 июня)",
-    "cancer": "Рак - четвёртый знак зодиака, Луна (с 22 июня по 22 июля)",
-    "leo": "Лев - пятый знак зодиака, солнце (с 23 июля по 21 августа)",
-    "virgo": "Дева - шестой знак зодиака, планета Меркурий (с 22 августа по 23 сентября)",
-    "libra": "Весы - седьмой знак зодиака, планета Венера (с 24 сентября по 23 октября)",
-    "scorpio": "Скорпион - восьмой знак зодиака, планета Марс (с 24 октября по 22 ноября)",
-    "sagittarius": "Стрелец - девятый знак зодиака, планета Юпитер (с 23 ноября по 22 декабря)",
-    "capricorn": "Козерог - десятый знак зодиака, планета Сатурн (с 23 декабря по 20 января",
-    "aquarius": "Водолей - одиннадцатый знак зодиака, планеты Уран и Сатурн (с 21 января по 19 февраля)",
-    "pisces": "Рыбы - двенадцатый знак зодиака, планеты Юпитер (с 20 февраля по 20 марта)"
+    "aries": Zodiac("Овен - первый знак зодиака, планета Марс (с 21 марта по 20 апреля)",
+                    "21-03", '20-04'),
+    "taurus": Zodiac("Телец - второй знак зодиака, планета Венера (с 21 апреля по 21 мая)",
+                     "21-04", "21-05"),
+    "gemini": Zodiac("Близнецы - третий знак зодиака, планета Меркурий (с 22 мая по 21 июня)",
+                     "22-05", "21-06"),
+    "cancer": Zodiac("Рак - четвёртый знак зодиака, Луна (с 22 июня по 22 июля)",
+                     "22-06", "22-07"),
+    "leo": Zodiac("Лев - пятый знак зодиака, солнце (с 23 июля по 21 августа)",
+                  "23-07", "21-08"),
+    "virgo": Zodiac("Дева - шестой знак зодиака, планета Меркурий (с 22 августа по 23 сентября)",
+                    "22-08", "23-09"),
+    "libra": Zodiac("Весы - седьмой знак зодиака, планета Венера (с 24 сентября по 23 октября)",
+                    "24-09", "23-10"),
+    "scorpio": Zodiac("Скорпион - восьмой знак зодиака, планета Марс (с 24 октября по 22 ноября)",
+                      "24-10", "22-11"),
+    "sagittarius": Zodiac("Стрелец - девятый знак зодиака, планета Юпитер (с 23 ноября по 22 декабря)",
+                          "23-11", "22-12"),
+    "capricorn": Zodiac("Козерог - десятый знак зодиака, планета Сатурн (с 23 декабря по 20 января",
+                        "23-12", "20-01"),
+    "aquarius": Zodiac("Водолей - одиннадцатый знак зодиака, планеты Уран и Сатурн (с 21 января по 19 февраля)",
+                       "21-01", "19-02"),
+    "pisces": Zodiac("Рыбы - двенадцатый знак зодиака, планеты Юпитер (с 20 февраля по 20 марта)",
+                     "20-02", "20-03")
 }
 
 types_zodiac = {
@@ -23,7 +38,6 @@ types_zodiac = {
     'air': ['pisces', 'scorpio', 'cancer'],
     'water': ['libra', 'aquarius', 'gemini']
 }
-
 
 def index(request):
     zodiacs = list(zodiac_dict)
@@ -40,9 +54,9 @@ def index(request):
 
 
 def get_info_about_sign_zodiac(request, sign_zodiac: str):
-    descreption = zodiac_dict.get(sign_zodiac)
-    if descreption:
-        return HttpResponse(descreption)
+    zodiac_description = zodiac_dict.get(sign_zodiac).description
+    if zodiac_description:
+        return HttpResponse(zodiac_description)
     else:
         return HttpResponseNotFound(f'Unknown sign zodiac - {sign_zodiac}')
 
@@ -69,7 +83,7 @@ def get_types_zodiac(request):
     """
     return HttpResponse(response)
 
-def get_zodiacs_by_type(requst, type_zodiacs: str):
+def get_zodiacs_by_type(request, type_zodiacs: str):
     signs = types_zodiac[type_zodiacs]
     li_sign_elements = ''
     for sign in signs:
@@ -81,3 +95,14 @@ def get_zodiacs_by_type(requst, type_zodiacs: str):
         </ul>
         """
     return HttpResponse(response)
+
+def get_info_about_sign_zodiac_by_date(request, month: int, day: int):
+    try:
+        dt(dt.today().year, month, day)
+    except ValueError as exc:
+        return HttpResponse(str(exc).capitalize())
+    inp_date = f'{str(day).zfill(2)}-{str(month).zfill(2)}'
+    for descr, s_d, e_d in zodiac_dict.values():
+        if dt.strptime(s_d, '%d-%m') <= dt.strptime(inp_date, '%d-%m') <= dt.strptime(e_d, '%d-%m'):
+            return HttpResponse(descr)
+        continue
